fde1108311c58980c02a139133b353fd87dc59a6
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-22 14:28:58 +0300

    created functions to return a single pitch for a comment to be added

--- app/main/views.py
+++ app/main/views.py
@@ -49,3 +49,42 @@ def category(id):
 
     pitches=Pitch.get_pitches(id)
     return render_template('category.html', pitches=pitches, category=category)
+
+
+@main.route('/add/category', methods=['GET','POST'])
+@login_required
+def new_category():
+    """
+    View new group route function that returns a page with a form to create a category
+    """
+    
+    form = CategoryForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        new_category = PitchCategory(name = name)
+        new_category.save_category()
+
+        return redirect(url_for('.index'))
+
+    title = 'New category'
+    return render_template('new_category.html', category_form = form, title = title)
+
+
+#view single pitch alongside its comments
+@main.route('/view-pitch/<int:id>', methods=['GET', 'POST'])
+@login_required
+def view_pitch(id):
+    """
+    Function the returns a single pitch for comment to be added
+    """
+    print(id)
+    pitches = Pitch.query.get(id)
+    # pitches = Pitch.query.filter_by(id=id).all()
+
+    if pitches is None:
+        abort(404)
+    #
+    comment = Comments.get_comments(id)
+    return render_template('view-pitch.html', pitches = pitches, comment = comment, category_id = id)
+
