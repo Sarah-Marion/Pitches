940cc0240be2f0689173f7b42cbb9ac52d66b55a
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-22 12:38:17 +0300

    created a login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -32,3 +32,13 @@ class RegistrationForm(FlaskForm):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That user name is already taken. Try another one')
 
+
+#login class  takes three inputs from the user
+class LoginForm(FlaskForm):
+    email = StringField('Your email address', validators=[Required(),Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
+
+
