419a85d3ab9fa8273b26d9b1a2199bd17229204a
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-22 13:06:47 +0300

    created function to check if the login form is validated

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,11 +10,11 @@ from ..models import User
 @auth.route('templates/auth/reqister',methods=['GET','POST'])
 def register():
     """
-    Function that registaers the users
+    Function that registers the users
     """
-    form =RegistrationForm()
+    form = RegistrationForm()
     if form.validate_on_submit():
-        user =User(email=form.email.data,username=form.username.data,password=form.password.data)
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
         db.session.add(user)
         db.session.commit()
 
@@ -22,4 +22,25 @@ def register():
 
     title = "Registration"
 
-    return render_template('auth/register.html',registration_form=form,title=title)
+    return render_template('auth/register.html', registration_form = form, title = title)
+
+
+# Login function
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    """
+    Function that checks if the form is validated
+    """
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next')or url_for('main.index'))
+
+        flash('Invalid Username or Password')
+
+    title = "60sec Pitch|Login"
+    return render_template('auth/login.html', login_form = login_form, title = title)
+
+
