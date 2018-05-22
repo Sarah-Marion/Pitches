916e226a2a6ac037a6ca0757682ea2553c6f9a94
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-22 22:02:32 +0300

    made changes to the the authentication

--- app/auth/views.py
+++ app/auth/views.py
@@ -44,3 +44,9 @@ def login():
     return render_template('auth/login.html', login_form = login_form, title = title)
 
 
+#logout function
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))# redirects user to the main page of the app after successful logout
