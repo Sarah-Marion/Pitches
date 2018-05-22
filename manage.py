2309c92541a9f3ac0d4741e3209f5ba10424191e
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-22 21:57:56 +0300

    changed port

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User,Pitch,Comments,PitchCategory
+from app.models import User, Pitch, Comments, PitchCategory
 
 from flask_migrate import Migrate, MigrateCommand
 
@@ -8,7 +8,7 @@ from flask_migrate import Migrate, MigrateCommand
 app = create_app('development')
 
 manager = Manager(app)
-manager.add_command('server', Server)
+manager.add_command('server', Server(port=4200))
 
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
@@ -16,7 +16,9 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
-    """Run unit tests."""
+    """
+    Run unit tests.
+    """
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
@@ -25,7 +27,7 @@ def test():
 @manager.shell
 def make_shell_context():
     return dict(app=app, db=db, User=User, Pitch = Pitch, Comments=Comments, PitchCategory=PitchCategory)
-    pass
+    # pass
 
 
 if __name__ == '__main__':
