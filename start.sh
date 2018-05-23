793913ba5eb2b7b96862c9025a480d723a506d90
Sarah-Marion <devsarahmarion@gmail.com>
2018-05-23 08:12:47 +0300

    deployment to heroku

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY="5047aa27156e4b7f0a8c7dd7dc6ef2afa449fbd0b78226bfd538d03a6f2d2514b5dea9412baff14ff535f6f9b1db6e9260c6"
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/pitch'
+export MAIL_USERNAME='devsarahmarion@gmail.com'
+export MAIL_PASSWORD='kachoochoo420'
+
+python3.6 manage.py server
